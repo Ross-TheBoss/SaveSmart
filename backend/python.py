6978c5afed26b563c9812555a3ea66f2bab88d20
rezneac <rezneacanatolie@gmail.com>
2025-02-09 09:12:01 +0000

    Commiting my search product parser

--- backend/python.py
+++ backend/python.py
@@ -5,7 +5,7 @@ import PIL.Image
 
 image = PIL.Image.open('./image.png')
 
-client = genai.Client(api_key="API_KEY")
+client = genai.Client(api_key="AIzaSyBPlLN0RvHJQlCB8mBFVzE0aHCQlX_HFVg")
 response = client.models.generate_content(
     model="gemini-2.0-flash",
     contents=["What is this image? Break down every position also show how many times each producs is bought, containing name ,price only and the lowes and highest price of an item! PLEASE GIVE ONLY WHAT I REQUESTED AS A JSON FILE!!! ALSO can you findcheaper alternatives to each item from uk supermarket stores", image])
